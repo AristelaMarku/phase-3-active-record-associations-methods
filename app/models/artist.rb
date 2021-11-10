22d9eca1f286771449dae53de8be55a14f66ef14
Aristela Marku <aristela.marku@gmail.com>
2021-11-10 10:54:03 +0100

    commit tree 5c3cdf6bf11fb47a177e41607428a9cb9bd43b9c

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,12 +7,12 @@ class Artist < ActiveRecord::Base
   end
 
   def song_count
-    #return the number of songs associated with the artist
-    self.songs.count
+   self.songs.count
+  end
+  
+  def genre_count
+   self.genres.count
   end
 
-  def genre_count
-    #return the number of genres associated with the artist
-    self.genres.count
-  end
+
 end
